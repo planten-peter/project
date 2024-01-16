89b0256049071c8b305ca423695126f8371d2bed
dendersen <dendersen@endersen.dk>
2024-01-16 09:45:33 +0100

    potential fix on memory crash

--- main/lvgl_demo_ui.c
+++ main/lvgl_demo_ui.c
@@ -8,15 +8,15 @@
 
 lv_obj_t* example_lvgl_demo_ui(lv_disp_t* disp, char* msg, lv_obj_t* old)
 {
-    if(old != NULL) lv_obj_clean(old);
-
     lv_obj_t* scr = lv_disp_get_scr_act(disp); //Get the current screen
-    lv_obj_t* label = lv_label_create(scr); //Create a label on the currently active screen
-    lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL_CIRCULAR); /* Circular scroll */
-    lv_label_set_text(label, msg);
+    if(old != NULL){
+        old = lv_label_create(scr); //Create a label on the currently active screen
+    }
+    lv_label_set_long_mode(old, LV_LABEL_LONG_SCROLL_CIRCULAR); /* Circular scroll */
+    lv_label_set_text(old, msg);
     
     /* Size of the screen (if you use rotation 90 or 270, please set disp->driver->ver_res) */
-    lv_obj_set_width(label, disp->driver->hor_res);
-    lv_obj_align(label, LV_ALIGN_TOP_MID, 0, 0);
-    return label;
+    lv_obj_set_width(old, disp->driver->hor_res);
+    lv_obj_align(old, LV_ALIGN_TOP_MID, 0, 0);
+    return old;
 }
